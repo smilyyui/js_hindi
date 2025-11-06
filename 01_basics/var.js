a6f524207d4089db7d6a8e3794458ae10a0f9af4
smilyyui <vickychacha1234@gmail.com>
2025-11-06 13:28:50 +0000

    changes

--- 01_basics/var.js
+++ 01_basics/var.js
@@ -6,7 +6,7 @@ let state;
 //account_Id=2 const can not be changed prefer not to use var because of isssue in block scope in functional scope
 //var is not declared it refer as undefined
 console.log(account_Id);
-accountEmail="abhi@papa.com"
+accountEmail="abhi@papa.co"
 accountpassword="5678"
 accountCity="Mathura"
 console.table([accountEmail,accountpassword,accountCity,account_Id,state])
