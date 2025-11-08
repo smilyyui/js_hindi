25e199c946fdc0da4ecfbe898c991661485f12d2
smilyyui <vickychacha1234@gmail.com>
2025-11-08 07:16:52 +0000

    string.js

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -2,3 +2,11 @@
 let a="munni badnam"
 let b="abhishek"
 console.log(`${a} huyi darling ${b} ke liye`)
+const newString="  hitesh   "
+console.log(newString)
+console.log(newString.trim())
+const url="https://hitesh.com/hitesh%20"
+console.log(url.replace('%20','-'))
+// string convert into array
+const s="abhi is good boy"
+console.log(s.split(' '))
